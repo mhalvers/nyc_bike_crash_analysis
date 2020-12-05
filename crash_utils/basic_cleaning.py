91a1ae8cc1de3a07572307650836273b8affed8a
Mark Halverson <mark.j.halverson@gmail.com>
2020-12-04 21:10:00 -0800

    Now fixes spelling error in contributing factors

--- crash_utils/basic_cleaning.py
+++ crash_utils/basic_cleaning.py
@@ -4,50 +4,56 @@ def basic_cleaning(df):
 
     """
 
-    ## imports
+    # imports
     import pandas as pd
     import numpy as np
+
 
     # first, remove LOCATION.  it is redundant with LATITUDE and LONGITUDE
     df.drop(columns = "LOCATION", inplace = True)
 
-    ## there are some zeros in the positions
-    mask = (df["LATITUDE"] < 35) | (df["LONGITUDE"]>-65)
 
+    # there are some zeros in the positions
+    mask = (df["LATITUDE"] < 35) | (df["LONGITUDE"]>-65)
     df.loc[mask,"LATITUDE"] = np.nan
     df.loc[mask,"LONGITUDE"] = np.nan
 
 
+    # correct the mis-spelling of "illness"
+    mask = df["CONTRIBUTING FACTOR VEHICLE 1"].str.fullmatch("illnes", case=False)
+    mask.fillna(False,inplace = True)
+    df.loc[mask,"CONTRIBUTING FACTOR VEHICLE 1"] = "Illness"
+    
+    
     # change date and time to datetime64
-
     crash_dt = df["CRASH DATE"] + " " + df["CRASH TIME"]
     crash_dt = pd.to_datetime(crash_dt)
-
     df.insert(0, "DATETIME", crash_dt)
-
     df.drop(columns = ["CRASH DATE", "CRASH TIME"], inplace = True)
 
 
-    ## OFF-STREET NAME
+    # OFF-STREET NAME
     #list( df["OFF STREET NAME"][~df["OFF STREET NAME"].isna()] )
     # basically a street address if there is one.  missing 90% of the values
-
     df.drop(columns="OFF STREET NAME", inplace = True)
 
 
-    # NUMBER OF PERSONS INJURED   and    NUMBER OF PERSONS KILLED
-    # almost everything is there.  convert to integer
-
+    # NUMBER OF PERSONS INJURED and NUMBER OF PERSONS KILLED
+    # almost everything is there.
     na_rows = df["NUMBER OF PERSONS INJURED"].isna()
     na_rows = df.loc[na_rows,:].index
 
+
     # the first one, 21634, has no injuries or deaths.  drop!
     df.drop(index=na_rows[0], inplace = True)
+
 
     # the second one, 26814, has a NUMER OF CYCLIST INJURED = 1, but nothing in the 
     df.loc[na_rows[1],"NUMBER OF PERSONS INJURED"] = 1
     df.loc[na_rows[1],"NUMBER OF PERSONS KILLED"] = 0
 
+
+    # convert the following columns to integer dtypes
     df["NUMBER OF PERSONS INJURED"] = df["NUMBER OF PERSONS INJURED"].astype("int")
     df["NUMBER OF PERSONS KILLED"] = df["NUMBER OF PERSONS KILLED"].astype("int")
 
@@ -70,9 +76,6 @@ def basic_cleaning(df):
     other_mask = (df["NUMBER OF MOTORIST INJURED"] > 0) | (df["NUMBER OF MOTORIST KILLED"] > 0)
     mask = cyclist_mask & other_mask
     df = df.loc[~mask,:]
-
-    # df.loc[mask,["VEHICLE TYPE CODE 1","VEHICLE TYPE CODE 2","VEHICLE TYPE CODE 3","VEHICLE TYPE CODE 4","VEHICLE TYPE CODE 5"]].head(50)
-
 
 
     ## finally, let's trim down the data to focus on predicting the
